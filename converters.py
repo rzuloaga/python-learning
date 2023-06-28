4f5c581b0eaeedbe01f461a3dadd632f3cc11667
Ramon Zuloaga Geli <ramon.zuloaga@trioteca.com>
2023-06-28 16:50:59 +0200

    FInished coursera course

---
+++ converters.py
@@ -0,0 +1,2 @@
+def kg_to_lbs(weight):
+    return weight / 0.45
---
+++ main.py
@@ -0,0 +1,14 @@
+# print("Say a number, I'll double it")
+
+# x = int(input())
+
+# print(2*x)
+
+print("Rabbits after 2 years")
+print("Input initial number of rabbits:")
+number_of_rabbits = int(input())
+print("Input years:")
+years = int(input())
+doublings = 12 * years // 3
+
+print(number_of_rabbits * 2 ** doublings)
